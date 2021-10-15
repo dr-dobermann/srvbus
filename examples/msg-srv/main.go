b1b463930f20c64943b439f8f1c5242933f20502
dr-dobermann <rgabitov@gmail.com>
2021-10-15 20:23:47 +0600

    Move example to inner directory
    examples/main.go -> examples/msg-srv/main.go

--- examples/msg-srv/main.go
+++ examples/msg-srv/main.go
@@ -16,6 +16,7 @@ func main() {
 	srv.AddTask(srvbus.SrvOutput, "Hello,", "world!")
 	sid, err := srv.AddTask(srvbus.SrvGetMessages, srvbus.MsgServerDef{ms, "hello", 1}, int64(2))
 	srv.AddTask(srvbus.SrvPutMessages, srvbus.MsgServerDef{ms, "hello", 1}, "sweet", "dober")
+
 	if err != nil {
 		log.Fatal("couldn't add GetMessage service due to", err.Error())
 	}
@@ -25,11 +26,14 @@ func main() {
 
 	srv.Run(ctx)
 	time.Sleep(10 * time.Second)
+
 	results, err := srv.GetResults(sid, false)
 	if err != nil {
 		log.Fatal("couldn't get results of Get Message service ", err)
 	}
+
 	fmt.Println("Got message(s):")
+
 	for _, m := range results {
 		ms := m.(srvbus.Message)
 		fmt.Printf("%v\n", ms.String())
